37b0a54037f63d032c6fc61e362b5297a35a1bd4
Ankeloff <ankeloff1@gmail.com>
2023-03-08 23:24:37 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,81 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+print("salom")
+# son = -72
+# if son < 0:
+#     print("Manfiy son")
+# else:
+#     print("Musbat son")
+
+
+
+
+
+# yosh = int(input("Yoshingiz nechida "))
+# if yosh <=4:
+#     # print('sizga kirish bepul ')
+#     narx = 0
+# elif yosh <= 12:
+#     narx = 5000
+# elif yosh <= 18:
+#     narx = 8000
+# else:
+#     narx = 10000
+# print(f"Sizga kirish {narx} sum")
+#
+
+
+# kun = input("Bugun nima kun?>>>")
+# choy = True
+# if kun.lower() == 'shanba' or kun.lower() == 'yakshanba':
+#     print ('bugun dam olish kuni.')
+# else:
+#     print('bugun ish kuni.')
+
+
+# narh = 15000  # mijoz 15 so'mga ovqat oldi
+# choy = True
+# salat = False
+# non = True
+# kompot = True
+# assorti = False
+# # Quyidagi har bir shart alohida tekshiriladi va bir-biriga bog'liq emas
+# if choy:  # agar choy olsa
+#     print("Mijoz choy oldi.")
+#     narh = narh + 3000
+# if salat:  # agar salat olsa
+#     print("Mijoz salat oldi.")
+#     narh = narh + 5000
+# if non:  # agar non olsa
+#     print("Mijoz non oldi.")
+#     narh = narh + 2000
+# if kompot:  # agar kompot olsa
+#     print("Mijoz kompot oldi.")
+#     narh = narh + 5000
+# if assorti:  # agar assorti olsa
+#     print("Mijoz assorti oldi.")
+#     narh = narh + 15000
+#
+# print(f"Jami {narh} so'm")
+
+
+
+
+
+
+menu = ['osh','qazonkabob','shashlik','norin','somsa']
+ovqat = input('Nima ovqat yeysiz?>>>')
+if ovqat.lower() in menu:
+    print('Buyurtma qabul qilindi.')
+else:
+    print('Afsuski bizda bunday ovqat yo\'q')
+print("liliya")
+
+
+
+numbers = [123,42,12,32]
+print(32 in numbers)
